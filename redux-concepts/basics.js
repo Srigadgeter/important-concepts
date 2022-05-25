640c6042ce3f483e266994239d5e6d029bfec5b8
Srigadgeter <srigadgets@gmail.com>
2022-05-25 13:03:13 +0530

    redux basics

---
+++ redux-concepts/basics.js
@@ -0,0 +1,22 @@
+// ********************************************* //
+// Redux, 3 Principles of Redux, Redux Flow
+// ********************************************* //
+
+// Definition
+// ----------
+// Redux is an open-source JavaScript library for managing and centralizing application state
+
+// Three Principles of Redux
+// -------------------------
+// 1. Single source of truth - The global state of your application is stored in an object tree within a single store
+// 2. State is read-only - The only way to change the state is to emit an action, an object describing what happened
+// 3. Changes are made with pure functions - To specify how the state tree is transformed by actions, you write pure reducers
+
+// Redux Flow
+// ----------
+// Check out the flow diagram (gif image) in the folder
+
+// References
+// ----------
+// https://redux.js.org/
+// https://redux.js.org/understanding/thinking-in-redux/three-principles
