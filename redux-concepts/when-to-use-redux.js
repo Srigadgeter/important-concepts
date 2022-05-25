fdddf3a357db8664c1677a5c3308e36c684e5097
Srigadgeter <srigadgets@gmail.com>
2022-05-25 13:15:53 +0530

    when to use redux

---
+++ redux-concepts/when-to-use-redux.js
@@ -0,0 +1,13 @@
+// ********************** //
+// When to use redux
+// ********************** //
+
+// Definition
+// ----------
+// 1. Same piece of application state needs to be mapped to multiple container components
+// 2. Too many props are being passed through multiple hierarchies of components
+// etc,.
+
+// References
+// ----------
+// https://medium.com/@fastphrase/when-to-use-redux-f0aa70b5b1e2
